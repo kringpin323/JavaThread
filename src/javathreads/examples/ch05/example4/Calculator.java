347c8c2bfc4572f2abcb2b6daf6ef00a58128a9a
kringpin323 <kringpin323@gmail.com>
2014-10-21 13:50:03 +0800

    ThreadLocal本地变量，难道我也歇菜了吗？

--- src/javathreads/examples/ch05/example4/Calculator.java
+++ src/javathreads/examples/ch05/example4/Calculator.java
@@ -3,22 +3,27 @@ package javathreads.examples.ch05.example4;
 import java.util.*;
 
 public abstract class Calculator {
+	// 由于 是 static的局部变量，对象本身是在thread间被共享
+	private static ThreadLocal<HashMap> results = new ThreadLocal<HashMap>() {
+		// 一般清情况下都会override 这个method，定义特定的 ThreadLocal数据结构
+		protected HashMap initialValue() {
+			return new HashMap();
+		}
+	};
 
-    private static ThreadLocal<HashMap> results = new ThreadLocal<HashMap>() {
-        protected HashMap initialValue() {
-            return new HashMap();
-        }
-    };
+	public Object calculate(Object param) {
+		// 就是说 results 是共享的，但是 results 里面的 HashMap却非共享，从输出结果来看，输出的HashMap的确
+		// 是 ThreadLocal 变量，作用而言，避免了线程间的交流，线程是安全了，但是却没有了线程间的同步
+		HashMap hm = results.get();
+		Object o = hm.get(param);
+		if (o != null)
+ 			return o;  // 如果这个线程的 HashMap里面有这个东西，直接返回这个东西，
+		// 如果没有， 会有 输出流
+		o = doLocalCalculate(param);
+		// 再写入
+		hm.put(param, o);
+		return o;
+	}
 
-    public Object calculate(Object param) {
-        HashMap hm = results.get();
-	Object o = hm.get(param);
-	if (o != null)
-	    return o;
-        o = doLocalCalculate(param);
-        hm.put(param, o);
-        return o;
-    }
-
-    protected abstract Object doLocalCalculate(Object param);
+	protected abstract Object doLocalCalculate(Object param);
 }
--- src/javathreads/examples/ch05/example4/CalculatorTest.java
+++ src/javathreads/examples/ch05/example4/CalculatorTest.java
@@ -3,7 +3,9 @@ package javathreads.examples.ch05.example4;
 public class CalculatorTest extends Calculator implements Runnable {
 
     public static void main(String[] args) {
-        int nThreads = Integer.parseInt(args[0]);
+    	
+//        int nThreads = Integer.parseInt(args[0]);
+    	int nThreads = Integer.parseInt("5");
         for (int i = 0; i < nThreads; i++) {
             Thread t = new Thread(new CalculatorTest());
             t.start();
@@ -12,6 +14,7 @@ public class CalculatorTest extends Calculator implements Runnable {
 
     public void run() {
         for (int i = 0; i < 30; i++) {
+        	// 一共 5 个线程， 每个线程运行 calculate 30次 0,1,2,3,4  各有 6 个，各计算 6 次
             Integer p = new Integer(i % 5);
             calculate(p);
         }
